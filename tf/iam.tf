f44fefa02f89c9fa4822890929991710930b43b5
Fayaaz <fayaaz.ahmed@gmail.com>
2025-07-27 17:50:04 +0100

    Fix s3 policies

--- tf/iam.tf
+++ tf/iam.tf
@@ -1,5 +1,5 @@
 // Input bucket policy for  user A
-resource "aws_s3_bucket_policy" "bucket_policy" {
+resource "aws_s3_bucket_policy" "input_bucket_policy" {
   bucket = aws_s3_bucket.input_bucket.id
   policy = jsonencode({
     Version = "2012-10-17"
@@ -7,7 +7,7 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       {
         Effect = "Allow"
         Principal = {
-          AWS = "${aws_iam_user.user_a.name}"
+          AWS = "${aws_iam_user.user_a.arn}"
         }
         Action = [
           "s3:*"
@@ -25,13 +25,13 @@ resource "aws_iam_user" "user_a" {
   name = "userA"
 }
 
-resource "aws_iam_user_policy_attachment" "iam_policy" {
+resource "aws_iam_user_policy_attachment" "iam_user_a_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
   user       = aws_iam_user.user_a.name
 }
 
 // Input bucket policy for  user B
-resource "aws_s3_bucket_policy" "bucket_policy" {
+resource "aws_s3_bucket_policy" "output_bucket_policy" {
   bucket = aws_s3_bucket.output_bucket.id
   policy = jsonencode({
     Version = "2012-10-17"
@@ -39,12 +39,10 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       {
         Effect = "Allow"
         Principal = {
-          AWS = "${aws_iam_user.user_b.name}"
+          AWS = "${aws_iam_user.user_b.arn}"
         }
         Action = [
           "s3:GetObject",
-          "s3:HeadObject",
-          "s3:GetBucketLocation",
           "s3:ListBucket"
         ]
         Resource = [
@@ -60,7 +58,7 @@ resource "aws_iam_user" "user_b" {
   name = "userB"
 }
 
-resource "aws_iam_user_policy_attachment" "iam_policy" {
+resource "aws_iam_user_policy_attachment" "iam_user_b_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
   user       = aws_iam_user.user_b.name
 }
